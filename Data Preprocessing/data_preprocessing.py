3df12513f053fc4cf7b1ab391747cd750ff66ec3
Adnan Rahman <adnanibnamotin@yahoo.com>
2018-07-21 00:43:45 +0600

    data preprocessing file update

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -8,8 +8,8 @@ Created on Fri Jul 20 19:57:14 2018
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import Imputer
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
 
 data = pd.read_csv('Data.csv')
@@ -24,4 +24,21 @@ X.iloc[:, 1:3] = imputer.fit_transform(X.iloc[:, 1:3])
 
 ## Solve categorical data problem
 le = LabelEncoder()
-X.iloc[:, 0:1] = le.fit_transform(X.iloc[:, 0:1])
+X.iloc[:, 0:1] = le.fit_transform(X.iloc[:, -3])
+
+## Create dummy varibles to solve categorial data issue
+ohe = OneHotEncoder(categorical_features=[0])
+X = ohe.fit_transform(X).toarray();
+
+y = le.fit_transform(y.iloc[:,0])
+
+
+
+## train and test model using train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+
+# Standardized the feature value
+ss = StandardScaler()
+X_train = ss.fit_transform(X_train)
+X_test = ss.transform(X_test)
